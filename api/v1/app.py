f50ac88f2778535de3b85175e48432c4ddcaaf1a
The Primotion Studio <oyedelenewton@gmail.com>
2024-04-26 15:17:00 +0100

    ...

--- api/v1/app.py
+++ api/v1/app.py
@@ -12,19 +12,21 @@ app.register_blueprint(app_views)
 
 @app.teardown_appcontext
 def teardown_appcontext(exception):
-    """close storage
+    """
+    close storage
     """
     storage.close()
 
 
 @app.errorhandler(404)
 def not_found(error):
-    """handles a JSON-formatted 404 status code response
+    """
+    handles a JSON-formatted 404 status code response
     """
     return make_response({"error": "Not found"}, 404)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     HBNB_API_HOST = getenv('HBNB_API_HOST') or '0.0.0.0'
     HBNB_API_PORT = getenv('HBNB_API_PORT') or 5000
-    app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True)
+    app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, debug=True, threaded=True)
