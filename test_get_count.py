170a2a845599fc0ad8dd7fe37eb1a9e65f52b768
The Primotion Studio <oyedelenewton@gmail.com>
2024-04-26 14:20:41 +0100

    file_storage

---
+++ test_get_count.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Test .get() and .count() methods
+"""
+from models import storage
+from models.state import State
+
+
+my_state = State("Rivers")
+storage.new(my_state)
+my_state = State("Osun")
+storage.new(my_state)
+print("All objects: {}".format(storage.count()))
+print("State objects: {}".format(storage.count(State)))
+# print("States".format(storage.all(State)))
+# first_state_id = list(storage.all(State).values())[0].id
+# print("First state: {}".format(storage.get(State, first_state_id)))
